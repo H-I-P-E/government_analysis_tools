2ac5d9756437d396cb4a140280c348cce7b5aaaf
Toby <tobias.jolly@education.gov.uk>
2018-09-18 17:52:31 +0100

    Created very basic shiny app

--- main.R
+++ main.R
@@ -3,19 +3,34 @@ library(dplyr)
 library(stringr)
 library(tidyr)
 library(purrr)
+library(shiny)
+library(ggplot2)
+
+build_dataset = F #Set to true to recreate dataset for app from raw data
 
 raw_data_folder <- 'raw_data' #Data from civil_service_jobs repo
-cooked_data_folder <- 'cooked_data'
-lookups_folder <- 'lookups'
-
+shiny_app_folder <- 'shiny-app'
+data_folder <- 'data'
 
 cleaned_data_csv <- file.path(raw_data_folder, 'cleaned_advert_data.csv')
 role_data_csv <- file.path(raw_data_folder, 'role_data.csv')
 grade_data_csv <- file.path(raw_data_folder, 'grade_data.csv')
 adverts_csv_name <- file.path(raw_data_folder, 'all_full_advert_data.csv')
 
-tool_counts_by_job_file <- file.path(cooked_data_folder, 'tool_counts_by_job.csv')
+regex_file <- 'tool_regexs.txt'
+tool_counts_file <- 'tool_counts_by_job.csv'
 
-regexs_table <- file.path(lookups_folder, 'tool_regexs.txt')
+#Paths for shiny app
+regexs_shiny_path <- file.path(data_folder, regex_file)
+tool_counts_by_job_shiny_path <- file.path(data_folder, tool_counts_file)
 
-source('R//create_data_set.R')
+#Pasths for command line
+tool_counts_by_job_file <- file.path(shiny_app_folder, tool_counts_by_job_shiny_path)
+regexs_table <- file.path(shiny_app_folder, tool_counts_by_job_shiny_path)
+
+
+#Check if data set exists - rebuild if necessary
+if(build_dataset){
+source('R//create_data_set.R')}
+
+runApp(shiny_app_folder)
---
+++ shiny-app/app.R
@@ -0,0 +1,52 @@
+# Load packages ----
+
+
+tools <- read_tsv(regexs_shiny_path)
+
+
+narrow_tool_counts <- read_csv(tool_counts_by_job_shiny_path) %>%
+  select(-length) %>%
+  gather(key='tool', value = 'count', -job_id, - job_department, -date_downloaded, -description_word_count) 
+
+# User interface ----
+ui <- fluidPage(
+  titlePanel("Civil service analytical tools"),
+  
+  sidebarLayout(
+    sidebarPanel(
+     selectInput('tool_selected', "Select a thing:",
+                 choices = tools$tool, multiple = T,
+                 selected = 'Excel'),
+     selectInput('departments_selected', "Select some departments:",
+                 choices = unique(narrow_tool_counts$job_department), multiple = T,
+                 selected = "DfE")
+    ),
+    
+    mainPanel(plotOutput("plot"))
+  )
+)
+
+# Server logic
+server <- function(input, output) {
+  get_data <- reactive(
+    narrow_tool_counts %>%
+      group_by(job_department, tool) %>%
+      summarise(sum = sum(count), 
+                job_count = sum(count>0), 
+                total_word_count = sum(description_word_count),
+                total_job_count = length(description_word_count)) %>%
+      mutate(proportion_of_jobs = job_count/total_job_count) %>%
+      ungroup() %>%
+      filter(job_department %in% input$departments_selected,
+             tool %in% input$tool_selected))
+
+  output$plot <- renderPlot(ggplot(
+    data = get_data(),
+    aes(x = job_department , y = proportion_of_jobs)) +
+    geom_bar(aes(fill = tool), stat="identity", position = "dodge") +
+    coord_flip()
+  )
+}
+
+# Run the app
+shinyApp(ui, server)
